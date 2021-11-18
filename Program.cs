738ccfaf1f9ab46ffa20c33ddea703b4f78dc314
pashwiniis <pashwini684@gmail.com>
2021-11-18 23:23:24 +0530

    welcome to line camparison problem statment

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LineComparisonProblem
+{
+    class geometry
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparison Computation Program on Master Branch");
+        }
+    }
+}
